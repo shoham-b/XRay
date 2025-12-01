c9c581b7d119b867c2c4d91041f2c6ba1e6ee7bd
fekeshet <ofekeshet@gmail.com>
2025-12-01 19:17:25 +0200

    sdds

--- src/xray/extension/viz.py
+++ src/xray/extension/viz.py
@@ -56,8 +56,8 @@ def plot_intensity_vs_radius(radii_mm, profile_percent, background_profile, prof
         max_r = np.max(radii_mm)
     plt.xlim(0, max_r)
     # User request: "in the radius plots make the y axis logaritmic"
-    plt.yscale('log')
-    # plt.ylim(0, 105) # ylim doesn't make sense with log scale if 0 is present. 
+    # plt.yscale('log')
+    plt.ylim(0, 105) # ylim doesn't make sense with log scale if 0 is present. 
     # Let's set a reasonable min for log scale, e.g. 0.1 or 1
     plt.ylim(bottom=0.1)
     plt.grid(True, alpha=0.5, linestyle='--')
